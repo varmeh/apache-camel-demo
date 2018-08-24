bee41dd11ffc1038677334d54015dc35992f9477
VarunM <varun.sagi20@gmail.com>
2018-08-24 20:13:47 +0530

    camel file copier app

---
+++ src/main/java/FileCopierApp.java
@@ -0,0 +1,23 @@
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileCopierApp {
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:data/inbox?noop=true")
+                        .to("file:data/outbox");
+            }
+        });
+
+        System.out.println("Starting Program");
+        context.start();
+        Thread.sleep(3000);
+        context.stop();
+        System.out.println("Exiting Program");
+    }
+}

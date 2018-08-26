f99a13efd1965d7d4de2215d6f65f57281b65656
VarunM <varun.sagi20@gmail.com>
2018-08-26 15:07:56 +0530

    @Handle annotation addition for reference.

--- src/main/java/com/order/manager/processor/BadProcessor.java
+++ src/main/java/com/order/manager/processor/BadProcessor.java
@@ -1,12 +1,16 @@
 package com.order.manager.processor;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Handler;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BadProcessor implements Processor {
 
+    // Handler annotation is required only when there are 2 methods of similar signature.
+    // In such case, this annotation avoids ambiguity & defines default method for invocation by BeanProcessor
+    @Handler
     public void process(Exchange exchange) throws Exception {
         System.out.println("Bad Order: " + exchange.getIn().getHeader("CamelFileName"));
     }
